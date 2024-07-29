b5810859b6761e163de4df64d64d736721bf8cf3
LaiKhai <laihongkhailai@gmail.com>
2024-07-29 14:59:28 +0700

    @29072024_update base structure and generate code_khai

--- flutter_base_project/lib/main.dart
+++ flutter_base_project/lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_base_project/core/di.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 GlobalKey<NavigatorState>? navigatorKey = GlobalKey<NavigatorState>();
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initAppModule();
   runApp(const MainApp());
 }
 
